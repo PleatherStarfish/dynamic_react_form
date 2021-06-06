49ac5533f1659a7f96c7e67d5bdab2ffdb835d5f
Daniel <lontano.composer@gmail.com>
2021-06-06 16:06:59 -0400

    Add a test bootstrap forum

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import { Form, Button, Container, Row, Col } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+import React from "react";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+          <Container>
+              <Row className="justify-content-md-center">
+                  <Col xs={12} sm={12} md={8} lg={6} xl={6}>
+                      <div className="front-page-box">
+                          <h1>Please tell us more...</h1>
+                          <Form>
+                              <Form.Group controlId="formBasicEmail">
+                                  <Form.Label>Email address</Form.Label>
+                                  <Form.Control type="email" placeholder="Enter email" />
+                                  <Form.Text className="text-muted">
+                                      We'll never share your email with anyone else.
+                                  </Form.Text>
+                              </Form.Group>
+
+                              <Form.Group controlId="formBasicPassword">
+                                  <Form.Label>Password</Form.Label>
+                                  <Form.Control type="password" placeholder="Password" />
+                              </Form.Group>
+                              <Form.Group controlId="formBasicCheckbox">
+                                  <Form.Check type="checkbox" label="Check me out" />
+                              </Form.Group>
+                              <Button variant="primary" type="submit">
+                                  Submit
+                              </Button>
+                          </Form>
+                      </div>
+                  </Col>
+              </Row>
+          </Container>
+      </div>
   );
 }
 
