7143d2288d6c8ec70f839bfe62d27fecd8721778
Daniel <lontano.composer@gmail.com>
2021-06-06 17:30:10 -0400

    Print form to console after submit

--- src/components/Form.js
+++ src/components/Form.js
@@ -9,18 +9,39 @@ function MainForm() {
     // Set initial state from array
     const [formContent, setFormContent] = useState(FormState);
 
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        for (let i = 0; i< event.target.length; i++) {
+
+            // Check if target is checkbox
+            if (event.target[i].type !== "checkbox") {
+                console.log(event.target[i].value)
+            }
+            else {
+                console.log(event.target[i].checked)
+            }
+        }
+    };
+
     const formGroupsToRender = formContent.map((value, index) => (
         <>
             <Form.Group controlId={value.name}>
-                <Form.Label className="main-form-label sr-only">{value.human_label}</Form.Label>
+
+                {value.type !== "checkbox"
+                    ? <Form.Label className="main-form-label sr-only">{value.human_label}</Form.Label>
+                    : <Form.Label className="main-form-label">{value.human_label}</Form.Label>}
+
                 <Form.Control type={value.type} placeholder={value.human_label}/>
             </Form.Group>
         </>
     ));
 
     return (
-        <Form>
+        <Form onSubmit={handleSubmit}>
             {formGroupsToRender.length > 0 ? formGroupsToRender : <Alert key="wait-alert" variant="primary">Please wait a moment while we look up your form.</Alert>}
+            <Button variant="primary" type="submit">
+                Submit
+            </Button>
         </Form>
     )
 }
