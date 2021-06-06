e08103255850cd8cc17c8fccd027f88186be58da
Daniel <lontano.composer@gmail.com>
2021-06-06 16:20:11 -0400

    Move MainForm to components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { Form, Button, Container, Row, Col } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
+import MainForm from './components/Form.js'
 import React from "react";
 
 
@@ -11,27 +12,8 @@ function App() {
               <Row className="justify-content-md-center">
                   <Col xs={12} sm={12} md={8} lg={6} xl={6}>
                       <div className="front-page-box">
-                          <h1>Please tell us more...</h1>
-                          <Form>
-                              <Form.Group controlId="formBasicEmail">
-                                  <Form.Label>Email address</Form.Label>
-                                  <Form.Control type="email" placeholder="Enter email" />
-                                  <Form.Text className="text-muted">
-                                      We'll never share your email with anyone else.
-                                  </Form.Text>
-                              </Form.Group>
-
-                              <Form.Group controlId="formBasicPassword">
-                                  <Form.Label>Password</Form.Label>
-                                  <Form.Control type="password" placeholder="Password" />
-                              </Form.Group>
-                              <Form.Group controlId="formBasicCheckbox">
-                                  <Form.Check type="checkbox" label="Check me out" />
-                              </Form.Group>
-                              <Button variant="primary" type="submit">
-                                  Submit
-                              </Button>
-                          </Form>
+                          <h1 className="front-page-h1">Please tell us more...</h1>
+                          <MainForm />
                       </div>
                   </Col>
               </Row>
